52fd1ccb288224208ae34c7af573e22144ceb45a
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-12-17 16:23:32 +0100

    Re add build user

--- android/scripts/release/release-config.sh
+++ android/scripts/release/release-config.sh
@@ -5,6 +5,5 @@
 # Where the release scripts and programs store temporary data
 export DATA_DIR="$HOME/.local/share/mullvad-release-android"
 
-# Mullvad code signing key and fingerprint
-export MULLVAD_CODE_SIGNING_KEY_PATH="../../../ci/keys/1.mullvad_signing.pub"
-export MULLVAD_CODE_SIGNING_KEY_FINGERPRINT="A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF"
+# The user on the buildserver that builds and uploads artifacts to the cdn servers
+export BUILDSERVER_BUILDUSER="build"
